15a9c9cd2ccd76ab613d7bb562c5900d6163ce0a
Artūrs Mekšs <arturs.mekss@tieto.com>
2014-01-29 23:05:55 +0200

    Fix: pass handling to next strategy instead of fail! if custom strategy didn't succeed

--- lib/devise/strategies/custom_authenticatable.rb
+++ lib/devise/strategies/custom_authenticatable.rb
@@ -15,6 +15,18 @@ module Devise::Strategies
       end
     end
 
+    # Override default Devise::Strategies::Authenticatable#validate method in order to *not* return fail! on flase,
+    # but pass handling to other strategies in stack, e.g default :database_authenticatable or other similar
+    # authentication will be failed with default message anyway if there are no strategies left
+    def validate(resource, &block)
+      if resource && resource.valid_for_authentication?(&block)
+        decorate(resource)
+        true
+      else
+        false
+      end
+    end
+
   end
 end
 
